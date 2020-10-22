e897dcf53a5a69f572f67085e0e8c6353610e8de
YEJI-324 <57705381+YEJI-324@users.noreply.github.com>
2020-10-23 04:03:42 +0900

    chart

--- app/src/main/java/com/jidokhants/mukyojeong/FragmentCalendarChart.java
+++ app/src/main/java/com/jidokhants/mukyojeong/FragmentCalendarChart.java
@@ -1,5 +1,6 @@
 package com.jidokhants.mukyojeong;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +11,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
 
 public class FragmentCalendarChart extends Fragment {
 
-    RadarChart radarChart;
 
     public static FragmentCalendarChart newInstance() {
         FragmentCalendarChart fragment = new FragmentCalendarChart();
@@ -25,10 +32,8 @@ public class FragmentCalendarChart extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar_chart, container, false);
-        radarChart = view.findViewById(R.id.radarChart);
-
-
 
         return view;
     }
+
 }
